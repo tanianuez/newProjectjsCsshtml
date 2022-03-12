1e994bf67bbbfeaaaa46e1057713e617244afb67
tanianuez <tanianue2003@gmail.com>
2022-03-12 22:25:20 +0100

    My First Push

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-file
+function newElement(){
+
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t); //add text to li
+}
+
+
